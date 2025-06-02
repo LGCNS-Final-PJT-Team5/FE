c746677c00d5ddd98c039afe15fea560847fce5b
jinsil296 <jinsil296@gmail.com>
2025-06-02 14:20:01 +0900

    test: 회원가입 컴포넌트 테스트 코드 추가

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-async-storage|zustand)/)',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/android/',
+    '<rootDir>/ios/',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/**/node_modules/**',
+  ],
 };
